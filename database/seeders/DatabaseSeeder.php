b7cac2b9405a3219b1c49af91c457b46383ab231
Brend Van Den Eynde <brend.vandeneynde@outlook.com>
2025-12-09 18:35:06 +0100

    Seeding toegevoegd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Subject;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Vakkken aanmaken
+        $wiskunde = Subject::create(['name' => 'Wiskunde']);
+        $engels = Subject::create(['name' => 'Engels']);
+        $chemie = Subject::create(['name' => 'Chemie']);
+        $geschiedenis = Subject::create(['name' => 'Geschiedenis']);
 
+        // Admin gebruiker aanmaken
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@ehb.be',
+            'password' => bcrypt('Password!321'), // Wachtwoord is 'Password!321'
+            'role' => 'admin',
+        ]);
+
+        // Tutor gebruiker aanmaken
+        $tutorBrendan = User::factory()->create([
+            'name' => 'Brendan Tutor',
+            'email' => 'tutor@lernie.test',
+            'password' => bcrypt('password'),
+            'role' => 'tutor',
+            'city' => 'Brussel',
+            'about_me' => 'Ik geef al 5 jaar les in exacte wetenschappen.',
+        ]);
+
+        // Koppel vakken aan tutor Brendan
+        $tutorBrendan->subjects()->attach([$wiskunde->id, $chemie->id]);
+
+        // Maak nog wat random tutors aan
+        $randomTutors = User::factory(5)->create([
+            'role' => 'tutor',
+        ]);
+
+        foreach($randomTutors as $tutor) {
+            $tutor->subjects()->attach($engels->id);
+        }
+
+        // Studenten aanmaken
+        User::factory(10)->create([
+            'role' => 'student',
+        ]);
+
+        // Specifieke student Brend
+        User::factory()->create([
+            'name' => 'Brend Van Den Eynde',
+            'email' => 'brend@lernie.test',
+            'password' => bcrypt('password'),
+            'role' => 'student',
         ]);
     }
 }
